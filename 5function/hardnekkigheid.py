9de473b858c233ef115d98b0c996393b5e9696fd
Ke <ke210zhang@gmail.com>
2020-10-27 17:49:34 +0100

    Week5

--- 5function/hardnekkigheid.py
+++ 5function/hardnekkigheid.py
@@ -1,1 +1,70 @@
 # https://dodona.ugent.be/nl/courses/359/series/3489/activities/582137927
+def multiplication(integer):
+    """
+    >>> multiplication(327)
+    42
+    >>> multiplication(42)
+    8
+    >>> multiplication(277777788888899)
+    4996238671872
+    """
+    product = 1
+    for number in str(integer):
+        product *= int(number)
+    return product
+
+def digital_root(integer):
+    """
+    >>> digital_root(327)
+    8
+    >>> digital_root(68889)
+    0
+    >>> digital_root(277777788888899)
+    0
+    """
+    while len(str(integer)) != 1:
+        integer = multiplication(integer)
+    return integer
+
+def persistence(integer):
+    """
+    >>> persistence(327)
+    2
+    >>> persistence(8)
+    0
+    >>> persistence(277777788888899)
+    11
+    """
+    count = 0
+    while len(str(integer)) != 1:
+        integer = multiplication(integer)
+        count += 1
+    return count
+def most_persistent(a, b):
+    """
+    >>> most_persistent(1, 100)
+    77
+    >>> most_persistent(100, 1000)
+    679
+    >>> most_persistent(1000, 10000)
+    6788
+    >>> most_persistent(277777788888000, 277777788889000)
+    277777788888899
+    """
+    greatest_persis = 0
+    i = 0
+    for integer in range(a, b+1):
+        if persistence(integer) > greatest_persis:
+            greatest_persis = persistence(integer)
+            i = integer
+        elif persistence(integer) == greatest_persis:
+            if integer <= i:
+                i = integer
+    return i
+
+
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
---
+++ 5function/mlecchita_vikalpa.py
@@ -0,0 +1,55 @@
+# https://dodona.ugent.be/nl/courses/359/series/3489/activities/534181732
+def iskey(k1, k2):
+    """
+    >>> iskey('THEQUICKBROWN', 'FXJMPSVLAZYDG')
+    True
+    >>> iskey('ABCDEFGHIJKLM', 'NOPQRSTUVW???')
+    False
+    >>> iskey('ABCDEFGHIJKLM', 'NOPQRSTUVW')
+    False
+    """
+    all = set(k1.upper() + k2.upper())
+    return len(all) == 26 and len(k1) == 13 and len(k2) == len(k1) and k1.isalpha() and k2.isalpha()
+def encode_character(m, k1, k2):
+    """
+    >>> encode_character('Q', 'THEQUICKBROWN', 'FXJMPSVLAZYDG')
+    'M'
+    >>> encode_character('v', 'THEQUICKBROWN', 'FXJMPSVLAZYDG')
+    'c'
+    >>> encode_character('?', 'THEQUICKBROWN', 'FXJMPSVLAZYDG')
+    '?'
+    """
+    if not m.isalpha():
+        return m
+    if m.isupper():
+        k1 = k1.upper()
+        k2 = k2.upper()
+        if k1.find(m) == -1:
+            return k1[k2.find(m)]
+        return k2[k1.find(m)]
+    m = m.upper()
+    k1 = k1.upper()
+    k2 = k2.upper()
+    if k1.find(m) == -1:
+        return k1[k2.find(m)].lower()
+    return k2[k1.find(m)].lower()
+
+def encode(string, k1, k2):
+    """
+    >>> encode('A person who does nothing will enjoy no happiness.', 'THEQUICKBROWN', 'FXJMPSVLAZYDG')
+    'B ujziyg dxy wyji gyfxsgn dskk jgeyo gy xbuusgjii.'
+    """
+    output = ''
+    for char in string:
+        output += encode_character(char, k1, k2)
+    return output
+
+def decode(string, k1, k2):
+    """
+    >>> decode('B ujziyg dxy wyji gyfxsgn dskk jgeyo gy xbuusgjii.', 'THEQUICKBROWN', 'FXJMPSVLAZYDG')
+    'A person who does nothing will enjoy no happiness.'
+    """
+    output = ''
+    for char in string:
+        output += encode_character(char, k1, k2)
+    return output
