1760e65d807861509febe2c7e836e132c295b3d2
Ke <ke210zhang@gmail.com>
2020-10-27 20:26:24 +0100

    Week5

--- 5function/tikcode.py
+++ 5function/tikcode.py
@@ -1,1 +1,58 @@
 # https://dodona.ugent.be/nl/courses/359/series/3489/activities/811362724
+def encode_letter(letter):
+    """
+    >>> encode_letter('V')
+    (5, 1)
+    >>> encode_letter('i')
+    (2, 4)
+    """
+    letter = letter.upper()
+    if letter == 'K':
+        letter = 'C'
+    alphabet = 'ABCDEFGHIJLMNOPQRSTUVWXYZ'
+    row = int(alphabet.find(letter)/5 + 1)
+    column = alphabet.find(letter) % 5 + 1
+    return (row, column)
+
+def decode_letter(row, column):
+    """
+    >>> decode_letter(5, 1)
+    'V'
+    >>> decode_letter(2, 4)
+    'I'
+    """
+    alphabet = 'ABCDEFGHIJLMNOPQRSTUVWXYZ'
+    index = (row-1) * 5 + column - 1
+    letter = alphabet[index]
+    return letter
+
+def encode(string):
+    """
+    >>> encode('VICTOR')
+    '..... . .. .... . ... .... .... ... .... .... ..'
+    >>> encode('Charlie')
+    '. ... .. ... . . .... .. ... . .. .... . .....'
+    """
+    list = []
+    for letter in string:
+        list.append('.' * encode_letter(letter)[0])
+        list.append('.' * encode_letter(letter)[1])
+    return ' '.join(list)
+
+def decode(string):
+    """
+    >>> decode('..... . .. .... . ... .... .... ... .... .... ..')
+    'VICTOR'
+    >>> decode('. ... .. ... . . .... .. ... . .. .... . .....')
+    'CHARLIE'
+    """
+    list_dot = string.split(' ')
+    coordinate = ''
+    output = ''
+    for item in list_dot:
+        coordinate += str(item.count('.'))
+    row_str = coordinate[::2]
+    column_str = coordinate[1::2]
+    for i in range(len(row_str)):
+        output += decode_letter(int(row_str[i]), int(column_str[i]))
+    return output
